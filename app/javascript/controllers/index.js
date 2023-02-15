1f3b8f49ff7abba4ffd3094fbbc2f13e3c70e1ed
hemroda <hemroda@gmail.com>
2023-02-15 17:14:47 +0100

    ui: notification flash message in admin

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,4 +5,7 @@
 import { application } from "./application"
 
 import HelloController from "./hello_controller"
+import Notification from 'stimulus-notification'
+
 application.register("hello", HelloController)
+application.register('notification', Notification)
