36ce643afa23caf492d35bb6eac950aa1f90b18e
hemroda <hemroda@gmail.com>
2023-02-16 00:03:52 +0100

    emails sending config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,26 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "casa_laterite_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    host: ENV["MAIL_HOST"],
+    protocol: "https"
+  }
+  config.action_mailer.smtp_settings = {
+    address: ENV["SES_SERVER"],
+    user_name:  ENV["SES_USERNAME"],
+    password:  ENV["SES_PASSWORD"],
+    port: '587',
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -91,4 +106,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  routes.default_url_options[:host] = "https://www.laterite.casa"
 end
