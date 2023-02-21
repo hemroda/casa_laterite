31d99ddcc9fb795a161d440bfd4433a4efdfce20
hemroda <hemroda@gmail.com>
2023-02-21 17:27:45 +0100

    add seed file for pproperty managers

---
+++ db/seeds/007_property_managers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+p "Seeding Property Managers"
+
+Property.all.each do |property|
+  Manager.create(manageable_type: "Property", manageable_id: property.id, user_id: rand(1..16), lead_manager: true,
+                 assigned_by: User.first)
+  if property.id.odd?
+    Manager.create(manageable_type: "Property", manageable_id: property.id, user_id: rand(1..16), lead_manager: true,
+                   assigned_by: User.find(rand(1..16)))
+  end
+end
