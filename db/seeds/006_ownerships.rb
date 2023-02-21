d46b85bc59b586507f3283de7c91f8dbd61a9866
hemroda <hemroda@gmail.com>
2023-02-21 16:40:34 +0100

    add seed file for ownerships

---
+++ db/seeds/006_ownerships.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+
+if Ownership.count.zero?
+  p "Seeding Owner"
+
+  Property.all.each do |property|
+    Ownership.create(account_id: rand(1..16), property_id: property.id, allocated_by: User.first)
+
+    Ownership.create(account_id: rand(1..16), property_id: property.id, allocated_by: User.find(rand(2..16))) if property.id.odd?
+  end
+end
